498f97937a0ab71f665d2bdfcf4e970d0149b274
Kranthi <116026056+EndrapuKranthiRaj@users.noreply.github.com>
2024-02-18 21:03:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(express.json())
 
 mongoose.connect(DATABASE)
 
+app.get('*', (req, res) => {
+  res.redirect('https://kranthi-raj-portfolio.vercel.app');
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
